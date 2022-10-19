42ecad443547b9cf1031669d7e279ce397aa7014
Chris Taylor <ctaylor@tactcomplabs.com>
2022-10-18 21:24:25 -0400

    initial import

---
+++ include/RAJA/policy/hpx/synchronize.hpp
@@ -0,0 +1,44 @@
+/*!
+ ******************************************************************************
+ *
+ * \file
+ *
+ * \brief   Header file for HPX synchronization.
+ *
+ ******************************************************************************
+ */
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-21, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#ifndef RAJA_synchronize_hpx_HPP
+#define RAJA_synchronize_hpx_HPP
+
+namespace RAJA
+{
+
+namespace policy
+{
+
+namespace hpx
+{
+
+/*!
+ * \brief Synchronize all HPX threads and tasks.
+ */
+RAJA_INLINE
+void synchronize_impl(const hpx_synchronize& sync)
+{
+    sync();
+}
+
+
+}  // end of namespace hpx
+}  // namespace policy
+}  // end of namespace RAJA
+
+#endif  // RAJA_synchronize_hpx_HPP
