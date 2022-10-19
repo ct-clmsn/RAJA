cdad5209f3e14bf089e91183dbfa8912f649f8bd
Chris Taylor <ctaylor@tactcomplabs.com>
2022-10-18 21:58:32 -0400

    initial import

---
+++ include/RAJA/policy/hpx/WorkGroup.hpp
@@ -0,0 +1,24 @@
+/*!
+ ******************************************************************************
+ *
+ * \file
+ *
+ * \brief   Header file containing RAJA Vtable and WorkRunner constructs.
+ *
+ ******************************************************************************
+ */
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016-21, Lawrence Livermore National Security, LLC
+// and RAJA project contributors. See the RAJA/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#ifndef RAJA_hpx_WorkGroup_HPP
+#define RAJA_hpx_WorkGroup_HPP
+
+#include "RAJA/policy/hpx/WorkGroup/Vtable.hpp"
+#include "RAJA/policy/hpx/WorkGroup/WorkRunner.hpp"
+
+#endif  // closing endif for header file include guard
